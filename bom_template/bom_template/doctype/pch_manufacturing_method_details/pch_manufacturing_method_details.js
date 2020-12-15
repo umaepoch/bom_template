45cf3effb48c78a69a2a5188e3ccb1e8ec7d1dd6
SureshAlien <sureshnaga96@gmail.com>
2020-12-15 17:34:00 +0530

    meth details

--- bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.js
+++ bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.js
@@ -6,3 +6,28 @@ frappe.ui.form.on('Pch Manufacturing Method Details', {
 
 	}
 });
+
+frappe.ui.form.on("Pch Manufacturing Method Details", "pch_method", function(frm, cdt, cdn) {
+	console.log("pch_process filed trigger  working")
+
+	var method_name = cur_frm.doc.pch_method;
+	var process_name = cur_frm.doc.pch_process;
+	var concat_val = method_name + "-" + process_name
+
+	if(method_name && process_name){
+		cur_frm.set_value("meth_pro_con", concat_val);
+	}
+	//cur_frm.set_value("target_warehouse",wh_json.outbound_warehouse);
+});
+
+
+
+
+frappe.ui.form.on("Pch Manufacturing Method Details", "pch_process", function(frm, cdt, cdn) {
+	var method_name = cur_frm.doc.pch_method;
+	var process_name = cur_frm.doc.pch_process;
+	var concat_val = method_name + "-" + process_name
+	if(method_name && process_name){
+		cur_frm.set_value("meth_pro_con", concat_val);
+	}
+});
