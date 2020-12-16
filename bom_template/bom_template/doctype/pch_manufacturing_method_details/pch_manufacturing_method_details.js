c727eaec65cd0442a3a0029f90dc7d289bd9593c
SureshAlien <sureshnaga96@gmail.com>
2020-12-16 18:08:47 +0530

    up

--- bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.js
+++ bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.js
@@ -1,11 +1,6 @@
 // Copyright (c) 2020, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Pch Manufacturing Method Details', {
-	refresh: function(frm) {
-
-	}
-});
 
 frappe.ui.form.on("Pch Manufacturing Method Details", "pch_method", function(frm, cdt, cdn) {
 	console.log("pch_process filed trigger  working")
@@ -21,13 +16,40 @@ frappe.ui.form.on("Pch Manufacturing Method Details", "pch_method", function(frm
 });
 
 
-
-
 frappe.ui.form.on("Pch Manufacturing Method Details", "pch_process", function(frm, cdt, cdn) {
+
 	var method_name = cur_frm.doc.pch_method;
 	var process_name = cur_frm.doc.pch_process;
-	var concat_val = method_name + "-" + process_name
 	if(method_name && process_name){
-		cur_frm.set_value("meth_pro_con", concat_val);
+		//validation_start
+		var process_flag = process_validation_against_method(method_name,process_name)
+		if (process_flag == 1){
+			frappe.msgprint("Selected Process : "+ process_name+" is already exist for method : "+method_name +".Please choose unique Process");
+			cur_frm.set_value("pch_process", null);
+		}else{
+			var concat_val = method_name + "-" + process_name
+			if(method_name && process_name){
+				cur_frm.set_value("meth_pro_con", concat_val);
+			}
+		}
+		//validation_end
 	}
 });
+
+function process_validation_against_method(method_name,process_name){
+	var process_flag ;
+	frappe.call({
+		method: 'bom_template.bom_template.doctype.pch_manufacturing_method_details.pch_manufacturing_method_details.process_validation_against_method',
+		async: false,
+		args:{
+			"method_name":method_name,
+			"process_name":process_name
+		},
+		callback: function(r) {
+			 if (r.message) {
+				 process_flag = r.message;
+				}
+		}
+    });
+		return process_flag;
+}
--- bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.py
+++ bom_template/bom_template/doctype/pch_manufacturing_method_details/pch_manufacturing_method_details.py
@@ -8,3 +8,24 @@ from frappe.model.document import Document
 
 class PchManufacturingMethodDetails(Document):
 	pass
+
+@frappe.whitelist()
+def process_validation_against_method(method_name,process_name):
+	process_flag = 0
+	process_dic =  frappe.db.sql("""select pch_process from `tabPch Manufacturing Method Details` where pch_method=%s """,(method_name), as_dict=1)
+	process_dic_list = []
+	for process_row in process_dic:
+		process_dic_list.append(process_row["pch_process"])
+	if process_name in process_dic_list:
+		process_flag = 1
+	return process_flag
+
+
+
+@frappe.whitelist()
+def get_process_list():
+
+	process_dic_list = []
+	for process_row in process_dic:
+		process_dic_list.append(process_row["process_name"])
+	return process_dic_list
