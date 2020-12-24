9d5246ebf248ed059504aef815dcdda4cbb8cc77
SureshAlien <sureshnaga96@gmail.com>
2020-12-24 09:58:05 +0530

    mrec js file only sur

--- bom_template/bom_template/doctype/pch_manufacturing_record/pch_manufacturing_record.js
+++ bom_template/bom_template/doctype/pch_manufacturing_record/pch_manufacturing_record.js
@@ -59,23 +59,27 @@ frappe.ui.form.on("Pch Manufacturing Record", "get_required_items", function(frm
 frappe.ui.form.on("Pch Manufacturing Record", "start_process", function(frm, cdt, cdn) {
 	var location_name = cur_frm.doc.location;
 	var start_process_temp = cur_frm.doc.start_process;
-	var process_name = get_process_name(start_process_temp)
-	console.log("process_name",process_name)
-	var outbound_warehouse_temp = get_wh_ac_to_location(location_name,"outbound_warehouse",process_name);
-	console.log("outbound_warehouse_temp",outbound_warehouse_temp)
-	cur_frm.set_value("outbound_warehouse", outbound_warehouse_temp);
+	if (location_name && start_process_temp){
+		var process_name = get_process_name(start_process_temp)
+		console.log("process_name",process_name)
+		var outbound_warehouse_temp = get_wh_ac_to_location(location_name,"outbound_warehouse",process_name);
+		console.log("outbound_warehouse_temp",outbound_warehouse_temp)
+		cur_frm.set_value("outbound_warehouse", outbound_warehouse_temp);
+	}
+
 });
 
 frappe.ui.form.on("Pch Manufacturing Record","end_process",function(frm,cdt,cdn){
 	var validation_flag = end_process_field_validation();
 	if (validation_flag == 1){
 		var location_name = cur_frm.doc.location;
-		var start_process_temp = cur_frm.doc.start_process;
-		var process_name = get_process_name(start_process_temp)
-		var inbound_warehouse_temp = get_wh_ac_to_location(location_name,"inbound_warehouse",process_name);
-		cur_frm.set_value("receiving_warehouse", inbound_warehouse_temp);
+		var end_process_temp = cur_frm.doc.end_process;
+		if (location_name && end_process_temp){
+			var process_name = get_process_name(end_process_temp)
+			var inbound_warehouse_temp = get_wh_ac_to_location(location_name,"inbound_warehouse",process_name);
+			cur_frm.set_value("receiving_warehouse", inbound_warehouse_temp);
+		}
 	}
-
 });
 
 
