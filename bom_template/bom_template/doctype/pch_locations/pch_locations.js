fe4cffe47b4486221d2bf38726522a7063dd8457
AkashMunikoti <akashmunikoti@gmail.com>
2020-12-28 17:11:25 +0530

    pch locations js py

--- bom_template/bom_template/doctype/pch_locations/pch_locations.js
+++ bom_template/bom_template/doctype/pch_locations/pch_locations.js
@@ -32,6 +32,211 @@ frappe.ui.form.on("Pch Locations", "get_process", function(frm, cdt, cdn) {
 
 });
 
+
+
+//Ak
+//Code for checking Inbound and Outbound within the Doctype p1
+frappe.ui.form.on("Pch Locations Child",{
+inbound_warehouse:function (frm, doctype, name) {
+var d = locals[doctype][name];
+var ib_list=[];
+var details=cur_frm.doc.locations_child;
+for(var i=0;i<details.length;i++)
+	{
+		if(i==0)
+		{
+		   continue;
+		
+		}
+		else
+		{
+    			console.log("Enter")
+    			var ib_prev=details[i-1]["inbound_warehouse"];
+    			var ib_current=details[i]["inbound_warehouse"];
+    			var ob_prev=details[i-1]["outbound_warehouse"];
+    			var ob_current=details[i]["outbound_warehouse"];
+    			
+    			if(ib_current===ob_prev)
+    			{
+    			
+    			   frappe.msgprint("This Inbound Warehouse has already been used in the previous row");
+    			   console.log(details[i]["inbound_warehouse"]);
+    			     details[i]["inbound_warehouse"]="";
+    			}
+    			
+    			else
+    			{
+    			 	console.log("Nothing");
+    			}
+    			
+              }
+        }
+
+	
+}
+});
+
+
+//Code for checking Inbound and Outbound within the Doctype p2
+frappe.ui.form.on("Pch Locations Child",{
+outbound_warehouse:function (frm, doctype, name) {
+var d = locals[doctype][name];
+var ib_list=[];
+var details=cur_frm.doc.locations_child;
+for(var i=0;i<details.length;i++)
+	{
+		if(i==0)
+		{
+		   continue;
+		
+		}
+		else
+		{
+    			console.log("Enter")
+    			var ib_prev=details[i-1]["inbound_warehouse"];
+    			var ib_current=details[i]["inbound_warehouse"];
+    			var ob_prev=details[i-1]["outbound_warehouse"];
+    			var ob_current=details[i]["outbound_warehouse"];
+    			
+    			 if(ob_current===ib_prev)
+    			{
+    			    
+    			   frappe.msgprint("This outbound warehouse name has already been used in the previous row");
+    			   console.log(details[i]["outbound_warehouse"]);
+    			   details[i]["outbound_warehouse"]="";
+    			}
+    			else
+    			{
+    			 	console.log("Nothing");
+    			}
+    			
+              }
+        }
+
+	
+}
+});
+
+
+//Function for checking inbound warehouse across the records
+frappe.ui.form.on("Pch Locations Child",{
+	inbound_warehouse:function(frm,doctype,name){
+	
+
+	
+	var d=locals[doctype][name];
+	var ib_wh=d.inbound_warehouse;
+	console.log(ib_wh);
+	//var is_unique_flag;
+	var is_unique_flag=is_unique_warehouse(ib_wh,'inbound');
+	console.log(is_unique_flag);
+	if(is_unique_flag===0)
+	{
+		 frappe.msgprint('This Inbound Warehouse is already active. Please re-enter a new Inbound Warehouse ');
+               
+               	d.inbound_warehouse="";
+	}
+	
+	
+    
+   }
+	
+	
+  
+});
+
+//Function for checking outbound warehouse across the records
+frappe.ui.form.on("Pch Locations Child",{
+	outbound_warehouse:function(frm,doctype,name){
+	
+
+
+	//console.log(company_list);
+	var d=locals[doctype][name];
+	
+	var ob_wh=d.outbound_warehouse;
+	console.log(ob_wh);
+	//var is_unique_flag;
+	 var is_unique_flag=is_unique_warehouse(ob_wh,'outbound');
+	console.log(is_unique_flag);
+	if(is_unique_flag===0)
+	{
+		 frappe.msgprint('This Outbound Warehouse is already active. Please re-enter a new Outbound Warehouse ');
+               
+               	d.outbound_warehouse="";
+	}
+	
+	
+    
+   }
+        
+	
+ 
+});
+
+frappe.ui.form.on("Pch Locations",{
+company_name:function(frm,cdt,cdn){
+
+  var comp=cur_frm.doc.company_name;
+  var ww=get_warehouses_list(comp);
+  //console.log(ww);
+  
+        
+        
+        //Filtering Outbound warehouse based on company
+        cur_frm.set_query("outbound_warehouse", "locations_child", function(frm, cdt, cdn) {
+    			//console.log(wh_n);
+            return {
+                "filters": [
+                    ["Warehouse", "name", "in", ww]
+                    
+                   
+        
+                ]
+            }
+		});
+		
+        cur_frm.refresh_field("locations_child");
+        cur_frm.refresh_field("outbound_warehouse");
+        
+        var a_list=get_account_details(comp);
+        
+       //Filtering Accounts based on company
+        cur_frm.set_query("difference_account", function(frm, cdt, cdn) {
+    			//console.log(wh_n);
+            return {
+                "filters": [
+                    ["Account", "name", "in", a_list]
+                    
+                   
+        
+                ]
+            }
+		});
+		
+        cur_frm.refresh_field("Pch Locations");
+        cur_frm.refresh_field("difference_account");
+        
+        
+        //Filtering inbound warehouse based on company 
+  cur_frm.set_query("inbound_warehouse", "locations_child", function(frm, cdt, cdn) {
+    			//console.log(wh_n);
+            return {
+                "filters": [
+                    ["Warehouse", "name", "in", ww]
+                    
+                   
+        
+                ]
+            }
+		});
+		
+        cur_frm.refresh_field("locations_child");
+        cur_frm.refresh_field("inbound_warehouse");
+        
+  }
+  });
+
 function get_process_list(){
 	var process_list ;
 	frappe.call({
@@ -56,3 +261,77 @@ function set_child_table( items_list){
 
 	refresh_field("locations_child");
 } //end of set_child_table
+
+
+function get_account_details(company_name){
+
+	var accounts=[]
+	frappe.call({
+		method:"bom_template.bom_template.doctype.pch_locations.pch_locations.get_account_details",
+		args:{
+		
+			"company_name":company_name
+		
+		},
+		async:false,
+		callback:function(r){
+		
+			accounts=r.message
+		}
+	
+	});
+	
+	return accounts
+
+}
+
+function is_unique_warehouse(warehouse_name,warehouse_type){
+
+	var is_unique_flag;
+	 frappe.call({
+               
+        method:"bom_template.bom_template.doctype.pch_locations.pch_locations.check_unique_warehouse",
+        args:{
+            
+            "warehouse_name":warehouse_name,
+            "warehouse_type":warehouse_type
+        },
+        async:false,
+        callback:function(r){
+            
+            is_unique_flag=r.message;
+           
+            
+        }
+    
+        
+    });
+
+	return is_unique_flag;
+
+}
+
+function get_warehouses_list(company_name){
+
+       var warehouse_list=[];
+	 frappe.call({
+               
+        method:"bom_template.bom_template.doctype.pch_locations.pch_locations.get_company_specific_warehouses",
+        args:{
+            
+            "company_name":company_name
+        },
+        async:false,
+        callback:function(r){
+            
+            warehouse_list=r.message;
+            
+           
+            
+        }
+    
+        
+    });
+
+       return warehouse_list;
+}
