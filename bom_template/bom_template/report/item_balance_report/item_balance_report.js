58eef72b33125b5741ea62f6a3874b3ce11ce7fd
SureshAlien <sureshnaga96@gmail.com>
2021-01-22 13:24:17 +0530

    dyn col js

--- bom_template/bom_template/report/item_balance_report/item_balance_report.js
+++ bom_template/bom_template/report/item_balance_report/item_balance_report.js
@@ -4,6 +4,14 @@
 
 frappe.query_reports["Item Balance Report"] = {
 	"filters": [{
+        "fieldname": "method_filter",
+        "label": __("Method"),
+        "fieldtype": "Link",
+        "options": "Pch Manufacturing Method",
+        "reqd": 1
+
+	},
+	{
         "fieldname": "item_made_fil",
         "label": __("Item Made"),
         "fieldtype": "Link",
