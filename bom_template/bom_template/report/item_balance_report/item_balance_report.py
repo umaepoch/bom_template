e51042ecac0f4b3d921a5b7c85740d49164043db
SureshAlien <sureshnaga96@gmail.com>
2021-01-22 13:21:06 +0530

    dyn col

--- bom_template/bom_template/report/item_balance_report/item_balance_report.py
+++ bom_template/bom_template/report/item_balance_report/item_balance_report.py
@@ -6,82 +6,42 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
-	columns = get_columns()
+	process_list = get_process_list(filters.get("method_filter"))
+	columns = get_columns(process_list)
+
 	mrec_data = get_mrec_data(filters)
-	process_list = ["Rakhi Making","Stone Fitting","Geenth","Packing"]
 	for item_made, item_json in mrec_data.items():
 		parent_row_dic = {"item_made_temp": item_made}
 
-		if item_json.get("Rakhi Making"):
-			for process_wise_rec_data_list in item_json.get("Rakhi Making"):
-				if process_wise_rec_data_list.get("manufacturing_record_type") == "Send Material for Manufacturing":
-					process_dic = {
-						"manufacturing_method": process_wise_rec_data_list.get("manufacturing_method"),
-						"rm_outbound_wh_qty": process_wise_rec_data_list.get("units_s_r"),
-						"rm_karigar_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-				elif process_wise_rec_data_list.get(
-						"manufacturing_record_type") == "Receive Material from Manufacturing":
-					process_dic = {
-						"rm_inbound_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-
-		if item_json.get("Geenth"):
-			for process_wise_rec_data_list in item_json.get("Geenth"):
-				if process_wise_rec_data_list.get("manufacturing_record_type") == "Send Material for Manufacturing":
-					process_dic = {
-						"manufacturing_method": process_wise_rec_data_list.get("manufacturing_method"),
-						"geenth_outbound_wh_qty": process_wise_rec_data_list.get("units_s_r"),
-						"geenth_karigar_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-				elif process_wise_rec_data_list.get(
-						"manufacturing_record_type") == "Receive Material from Manufacturing":
-					process_dic = {
-						"geenth_inbound_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-
-		if item_json.get("Stone Fitting"):
-			for process_wise_rec_data_list in item_json.get("Stone Fitting"):
-				if process_wise_rec_data_list.get("manufacturing_record_type") == "Send Material for Manufacturing":
-					process_dic = {
-						"manufacturing_method": process_wise_rec_data_list.get("manufacturing_method"),
-						"sf_outbound_wh_qty": process_wise_rec_data_list.get("units_s_r"),
-						"sf_karigar_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-				elif process_wise_rec_data_list.get(
-						"manufacturing_record_type") == "Receive Material from Manufacturing":
-					process_dic = {
-						"sf_inbound_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-
-		if item_json.get("Packing"):
-			for process_wise_rec_data_list in item_json.get("Packing"):
-				if process_wise_rec_data_list.get("manufacturing_record_type") == "Send Material for Manufacturing":
-					process_dic = {
-						"pack_outbound_wh_qty": process_wise_rec_data_list.get("units_s_r"),
-						"pack_karigar_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-				elif process_wise_rec_data_list.get(
-						"manufacturing_record_type") == "Receive Material from Manufacturing":
-					process_dic = {
-						"pack_inbound_wh_qty": process_wise_rec_data_list.get("units_s_r")
-					}
-					parent_row_dic.update(process_dic)
-
+		#dynmic col data
+		for process in process_list :
+			outbound_label = process + " " + "Outbound"
+			karigar_label = process + " " + "Karigar"
+			inbound_label = process + " " + "Inbound"
+			if item_json.get(process):
+				for process_wise_rec_data_list in item_json.get(process):
+					if process_wise_rec_data_list.get("manufacturing_record_type") == "Send Material for Manufacturing":
+						process_dic = {
+							"manufacturing_method": process_wise_rec_data_list.get("manufacturing_method"),
+							outbound_label: process_wise_rec_data_list.get("units_s_r"),
+							karigar_label: process_wise_rec_data_list.get("units_s_r")
+						}
+						parent_row_dic.update(process_dic)
+					elif process_wise_rec_data_list.get("manufacturing_record_type") == "Receive Material from Manufacturing":
+						process_dic = {
+							inbound_label: process_wise_rec_data_list.get("units_s_r")
+						}
+						parent_row_dic.update(process_dic)
 		data.append(parent_row_dic)
+		#dynmic col data
 
 	return columns, data
 
-def get_columns():
+def get_columns(process_list):
 	columns = []
-	for col in range(15):
+	range_temp = 2 + (len(process_list) * 3)
+
+	for col in range(range_temp):
 		columns.append("")
 
 
@@ -99,75 +59,40 @@ def get_columns():
 	"fieldtype": "Link",
 	"width": 160
 	}
+	last_col = 1
 
-	columns[2] = {
-	"label": "Rakhi Making Outbound",
-	"fieldname": "rm_outbound_wh_qty",
-	"width": 160
-	}
-	columns[3] = {
-	"label": "Rakhi Making Karigar",
-	"fieldname": "rm_karigar_wh_qty",
-	"width": 160
-	}
-	columns[4] = {
-	"label": "Rakhi Making Inbound",
-	"fieldname": "rm_inbound_wh_qty",
-	"width": 160
-	}
-	columns[5] = {
-	"label": "Geenth Outbound",
-	"fieldname": "geenth_outbound_wh_qty",
-	"width": 160
-	}
-	columns[6] = {
-	"label": "Geenth Karigar",
-	"fieldname": "geenth_karigar_wh_qty",
-	"width": 160
-	}
-	columns[7] = {
-	"label": "Geenth Inbound",
-	"fieldname": "geenth_inbound_wh_qty",
-	"width": 160
-	}
-	columns[8] = {
-	"label": "Stone Fitting Outbound",
-	"fieldname": "sf_outbound_wh_qty",
-	"width": 160
-	}
-	columns[9] = {
-	"label": "Stone Fitting Karigar",
-	"fieldname": "sf_karigar_wh_qty",
-	"width": 160
-	}
-	columns[10] = {
-	"label": "Stone Fitting Inbound",
-	"fieldname": "sf_inbound_wh_qty",
-	"width": 160
-	}
-	columns[11] = {
-	"label": "Packing Outbound",
-	"fieldname": "pack_outbound_wh_qty",
-	"width": 160
-	}
-	columns[12] = {
-	"label": "Packing Karigar",
-	"fieldname": "pack_karigar_wh_qty",
-	"width": 160
-	}
-	columns[13] = {
-	"label": "Packing Inbound",
-	"fieldname": "pack_inbound_wh_qty",
-	"width": 160
-	}
+	for process in process_list :
+		outbound_label = process + " " + "Outbound"
+		karigar_label =  process + " " + "Karigar"
+		inbound_label =  process + " " + "Inbound"
+		columns[last_col + 1] = {
+			"label": outbound_label,
+			"fieldname": outbound_label,
+			"width": 160
+		}
+		last_col += 1
 
+		columns[last_col + 1] = {
+			"label": karigar_label,
+			"fieldname": karigar_label,
+			"width": 160
+		}
+		last_col += 1
+
+		columns[last_col + 1] = {
+			"label": inbound_label,
+			"fieldname": inbound_label,
+			"width": 160
+		}
+		last_col += 1
+	#print "columns",columns
 	return columns
 
 def get_mrec_data(filters):
 	if filters.get("item_made_fil"):
 		mrec_data = frappe.db.sql("""select mrec.name,mrec.item_made,mrec.units_s_r,mrec.manufacturing_record_type,mrec.manufacturing_method,mrec.start_process,mmd.process_order,mmd.pch_process,mrec.outbound_warehouse,mrec.sub_contractor,mrec.target_warehouse,mrec.receiving_warehouse from `tabPch Manufacturing Record`    mrec,`tabPch Manufacturing Method Details` mmd where   mrec.docstatus = 1 and mrec.item_made = %s  and mmd.name = mrec.start_process order by mmd.process_order """, (filters.get("item_made_fil")), as_dict=1)
-	else:
-		mrec_data = frappe.db.sql("""select mrec.name,mrec.item_made,mrec.units_s_r,mrec.manufacturing_method,mrec.start_process,mmd.process_order,mmd.pch_process,mrec.outbound_warehouse,mrec.sub_contractor,mrec.target_warehouse,mrec.receiving_warehouse from `tabPch Manufacturing Record`    mrec,`tabPch Manufacturing Method Details` mmd where   mrec.docstatus = 1 and  mmd.name = mrec.start_process order by mmd.process_order """,as_dict=1)
+	#else:
+	#mrec_data = frappe.db.sql("""select mrec.name,mrec.item_made,mrec.units_s_r,mrec.manufacturing_method,mrec.start_process,mmd.process_order,mmd.pch_process,mrec.outbound_warehouse,mrec.sub_contractor,mrec.target_warehouse,mrec.receiving_warehouse from `tabPch Manufacturing Record`    mrec,`tabPch Manufacturing Method Details` mmd where   mrec.docstatus = 1 and  mmd.name = mrec.start_process order by mmd.process_order """,as_dict=1)
 
 	mrec_json = {}   #{"item1":{"process1":[query_data,query_data], "process2":[query_data,query_data] }}
 	for mrec in mrec_data :
@@ -182,7 +107,7 @@ def get_mrec_data(filters):
 		else: #no processes exist
 			process_dic ={mrec.get("pch_process"): [mrec] }
 			mrec_json[mrec["item_made"]]= process_dic
-	print ("mrec_json",mrec_json)
+	#print "mrec_json",mrec_json
 
 	return mrec_json
 
@@ -194,3 +119,11 @@ def get_conditions(filters):
 def get_stock_qty_in_wh(item_code,wh):
     stock_qty_in_wh = frappe.db.sql("""select actual_qty  from `tabBin` where item_code=%s and warehouse= %s""", (item_code,wh), as_dict=1)
     return stock_qty_in_wh[0]["actual_qty"] if  stock_qty_in_wh else 0
+
+def get_process_list(method_name):
+	process_dic = frappe.db.sql("""select DISTINCT pch_process  from `tabPch Manufacturing Method Details` where pch_method=%s """,(method_name), as_dict=1)
+	process_list = []
+	for process in process_dic:
+		process_list.append(process["pch_process"])
+	return process_list
+
